c7f53e23e1e68f524361cdc9a5da6769878c686e
Chafi Bennassar <bennasserchafi94@gmail.com>
2018-06-08 01:05:02 +0200

    new chages

--- Main.java
+++ Main.java
@@ -4,8 +4,8 @@ public class Main {
 	public static void main(String[] args) {
 		
 		for(int i =1; i<=10; i++){
-			System.out.print("Hello World this is new version ... " + i);
-		}
+			System.out.print("Helloqd World this is new version ... " + i);
+		}qzdzq
 
 	}
 
